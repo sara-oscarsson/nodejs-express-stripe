2b336131a3116348da3ed92910c97623e0ea8bb2
Ossian Gren <72518806+Grenhub@users.noreply.github.com>
2021-09-21 13:56:58 +0200

    products-localstorage

---
+++ public/products.js
@@ -0,0 +1,22 @@
+let products = [
+  {
+    id: 1,
+    price: 30000000,
+    model: "Jesko",
+    description:
+      "Jesko – the all-new megacar from Koenigsegg – is powered by a newly designed, 1280hp (1600hp on E85 in some markets), twin-turbocharged V8 engine that benefits from significant changes to the Agera engine it replaces.",
+    manufacturer: "Koenigsegg",
+    image:
+      "https://www.koenigsegg.com/wp-content/uploads/2019/02/JESKO_perspective_front_72dpi-e1550924120385-1024x575.jpg",
+  },
+  {
+    id: 2,
+    price: 1250000,
+    model: "AMG GLE Coupé",
+    description:
+      "A sporty and elegant coupé-design, excellent for terrain and dynamics typical to AMG: This is what makes the all new Mercedes-AMG GLE Coupé just that special.",
+    manufacturer: "Mercedes",
+    image:
+      "https://www.mercedes-benz.se/passengercars/mercedes-benz-cars/models/gle/coupe-c167/mercedes-amg/equipmentcomparison/_jcr_content/comparisonslider/par/comparisonslide_1298627266/exteriorImage.MQ6.12.20210430121452.jpeg",
+  },
+];
