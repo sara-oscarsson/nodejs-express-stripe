e5264a5660b3858669ee29312393ffb559f296db
Ossian Gren <72518806+Grenhub@users.noreply.github.com>
2021-09-23 15:54:05 +0200

    login-hashed-password

--- public/JS/cart.js
+++ public/JS/cart.js
@@ -1,91 +1,91 @@
-window.addEventListener('load', () => {
-    showWhatsInCart();
+window.addEventListener("load", () => {
+  showWhatsInCart();
 });
 
 //Connect to stripe with publishable key??
-let stripe = Stripe('pk_test_51Jc8njIyEKoWmrdFxr80HrBTOYFNs9uo8qNCRz6slhLv5CY6zGH622D2i1CXVU5WtCzapgHCXI4v96i9t6iJdp7j00UgqRplu5');
+let stripe = Stripe(
+  "pk_test_51Jc8njIyEKoWmrdFxr80HrBTOYFNs9uo8qNCRz6slhLv5CY6zGH622D2i1CXVU5WtCzapgHCXI4v96i9t6iJdp7j00UgqRplu5"
+);
 
-let buyButton = document.getElementById('buyBtn');
-buyButton.addEventListener('click', youAreNowBroke);
+let buyButton = document.getElementById("buyBtn");
+buyButton.addEventListener("click", youAreNowBroke);
 
 function showWhatsInCart() {
-    let currentCart = localStorage.getItem("cart")
-    currentCart = JSON.parse(currentCart)
-    
-    let productsInCart = document.querySelector("#productsFromCart");
-    if(currentCart == null){
-        productsInCart.innerText = "Nothing in cart.."
-        return
-    }
-    currentCart.forEach(cartItem => {
-        let card = document.createElement('div');
-        card.style.display = 'flex';
-        card.style.flexDirection = 'row';
-        card.style.justifyContent = 'space-between';
-        card.style.padding = '20px';
-        card.style.margin = '20px';
-        card.style.border = '1px solid black';
+  let currentCart = localStorage.getItem("cart");
+  currentCart = JSON.parse(currentCart);
 
-        let leftContent = document.createElement('div');
-        leftContent.style.display = 'flex';
-        leftContent.style.flexDirection = 'column';
+  let productsInCart = document.querySelector("#productsFromCart");
+  if (currentCart == null) {
+    productsInCart.innerText = "Nothing in cart..";
+    return;
+  }
+  currentCart.forEach((cartItem) => {
+    let card = document.createElement("div");
+    card.style.display = "flex";
+    card.style.flexDirection = "row";
+    card.style.justifyContent = "space-between";
+    card.style.padding = "20px";
+    card.style.margin = "20px";
+    card.style.border = "1px solid black";
 
-        
-        let title = document.createElement("h3");
-        title.innerText = cartItem.car.model;
-        
-        let quantity = document.createElement("h4");
-        quantity.innerText = "Antal: " + cartItem.quantity;
+    let leftContent = document.createElement("div");
+    leftContent.style.display = "flex";
+    leftContent.style.flexDirection = "column";
 
-        let rightContent = document.createElement('div');
-        rightContent.style.width = '100px';
-        rightContent.style.height = 'inherit';
+    let title = document.createElement("h3");
+    title.innerText = cartItem.car.model;
 
-        let carImage = document.createElement('img');
-        carImage.style.width = 'inherit';
-        carImage.style.height = 'inherit';
-        carImage.style.objectFit = 'cover';
+    let quantity = document.createElement("h4");
+    quantity.innerText = "Antal: " + cartItem.quantity;
 
-        carImage.src = cartItem.car.image;
-        
-        rightContent.appendChild(carImage);
-        leftContent.append(title, quantity);
-        card.append(leftContent, rightContent);
-        productsInCart.appendChild(card);
+    let rightContent = document.createElement("div");
+    rightContent.style.width = "100px";
+    rightContent.style.height = "inherit";
 
-    });
-    totalPriceCalculator();
+    let carImage = document.createElement("img");
+    carImage.style.width = "inherit";
+    carImage.style.height = "inherit";
+    carImage.style.objectFit = "cover";
+
+    carImage.src = cartItem.car.image;
+
+    rightContent.appendChild(carImage);
+    leftContent.append(title, quantity);
+    card.append(leftContent, rightContent);
+    productsInCart.appendChild(card);
+  });
+  totalPriceCalculator();
 }
 
 function totalPriceCalculator() {
-    let totalPriceDiv = document.getElementById('totalPrice');
-    let totalPrice = 0;
+  let totalPriceDiv = document.getElementById("totalPrice");
+  let totalPrice = 0;
 
-    let currentCart = localStorage.getItem("cart")
-    currentCart = JSON.parse(currentCart)
+  let currentCart = localStorage.getItem("cart");
+  currentCart = JSON.parse(currentCart);
 
-    currentCart.forEach(cartItem => {
-        let oneProductKind = Number(cartItem.quantity) * Number(cartItem.car.price);
-        totalPrice += oneProductKind;
-    });
-    totalPriceDiv.innerText = totalPrice + ':-';
+  currentCart.forEach((cartItem) => {
+    let oneProductKind = Number(cartItem.quantity) * Number(cartItem.car.price);
+    totalPrice += oneProductKind;
+  });
+  totalPriceDiv.innerText = totalPrice + ":-";
 }
 
 async function youAreNowBroke() {
-    let currentCart = localStorage.getItem("cart")
-    let respone = await fetch('/payment', {
-        headers: {'Content-Type': 'application/json'},
-        method: 'POST',
-        body: currentCart
-    })
+  let currentCart = localStorage.getItem("cart");
+  let respone = await fetch("/payment", {
+    headers: { "Content-Type": "application/json" },
+    method: "POST",
+    body: currentCart,
+  })
     .then((result) => {
-        return result.json();
+      return result.json();
     })
     .then((session) => {
-        console.log(session)
-        localStorage.setItem("sessionID", session.id)
-        localStorage.removeItem("cart");
-        return stripe.redirectToCheckout({sessionId: session.id});
+      console.log(session);
+      localStorage.setItem("sessionID", session.id);
+      localStorage.removeItem("cart");
+      return stripe.redirectToCheckout({ sessionId: session.id });
     })
     .catch((err) => console.error(err));
 }
